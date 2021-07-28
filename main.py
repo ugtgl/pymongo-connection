3ed9ce49c9f366c04c449e8b1e4800cb1b5d4db2
ugtgl <ugtgl@hotmail.com>
2021-07-28 20:41:36 +0300

    mongodb collection to pandas dataframe

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-#from pymongo import MongoClient
-#from pymongo.errors import CollectionInvalid
-#from collections import OrderedDict
 import pandas
 import pymongo
 from bson.json_util import dumps, loads
 import pandas as pd
 
-#client = pymongo.MongoClient("mongodb://localhost:27019/")
 client = pymongo.MongoClient('localhost', 27017)
 
 print("test 1 client")
@@ -24,27 +20,49 @@ print("test 4 query")
 query = { "continent": "Asia" }
 print(query)
 
-'''print("test 5")
-documents = collection.find(query)
-f = open("asia.json", "w")
-#for x in documents:
-#    f.write(x)
-f.write('[')
-for document in documents:
-    f.write(dumps(document))
-    f.write(',')
-f.write(']')
-f.close()
-print("dosyaya yazıldı")'''
-
 print("test 6 query sonucundan çıkan veriler dataframe'e")
 r = collection.find(query)
-l = list(r) # Converts object to list
-d = dumps(l) # Converts to String
-dict_needed = loads(d) # Serializes String and creates dictionary
+l = list(r)
+d = dumps(l)
+dict_needed = loads(d)
 #print(dict_needed)
 df = pandas.DataFrame.from_dict(dict_needed)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 print(df.columns)
 
+print("test 7 örnek uygulama")
+print("Veritabanını seçin (local)\n")
+for db in client.list_databases():
+    print(db)
+dbinput = input()
+selecteddb = client[dbinput]
+
+print("Collection seçin (deneme)\n")
+
+for collection in selecteddb.list_collection_names():
+    print(collection)
+collectioninput = input()
+selectedcollection = selecteddb[collectioninput]
+
+thedataframe = pandas.DataFrame.from_dict(loads(dumps(list(selectedcollection.find()))))
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    print("Bütün dataframe:",thedataframe)
+
+print("Yaş ortalamaları:",thedataframe["median_age"])
+# print("Query türünü girin (sayı olarak sadece 1 veya 2)")
+# print("1. Find\n2. Insert")
+# selectedqueryoption = int(input())
+#
+# if selectedqueryoption == 1:
+#     print("Arama querysini detaylı olarak girin. (örn {continent: Asia})")
+#     searchquery = dict(input())
+#     returnedquery = selectedcollection.find(searchquery)
+#     thelist = list(returnedquery)
+#     thestring = dumps(thelist)
+#     thedict = loads(thestring)
+#     thedataframe = pandas.DataFrame.from_dict(thedict)
+#     with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+#         print(thedataframe)
+# elif selectedqueryoption == 2:
+#     print()
